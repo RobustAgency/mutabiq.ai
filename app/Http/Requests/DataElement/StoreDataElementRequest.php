c49e436b74782f9615317090fa17ff58be0ecce6
Abdul Wahab <wshahid767@gmail.com>
2026-01-07 16:41:16 +0500

    Refactor sample_values field to use string type in StoreDataElementRequest and UpdateDataElementRequest; update DataElementFactory to generate sample values as sentences.

--- app/Http/Requests/DataElement/StoreDataElementRequest.php
+++ app/Http/Requests/DataElement/StoreDataElementRequest.php
@@ -37,7 +37,7 @@ class StoreDataElementRequest extends FormRequest
             'is_unique' => ['nullable', 'boolean'],
             'default_value' => ['nullable', 'string'],
             'validation_rule' => ['nullable', 'string'],
-            'sample_values' => ['nullable', 'array'],
+            'sample_values' => ['nullable', 'string'],
             'sensitivity' => ['required', 'string', Rule::enum(Sensitivity::class)],
             'contains_personal_data' => ['required', 'boolean'],
             'personal_data_type' => ['required_if:contains_personal_data,true', 'string', Rule::enum(PersonalDataCategory::class)],
