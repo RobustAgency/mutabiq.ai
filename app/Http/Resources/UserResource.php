d406fc703b34519d92e6f7b56c75f935dda22a00
Abdul Wahab <wshahid767@gmail.com>
2026-02-11 12:35:03 +0500

    feat: Update UserResource to format roles and permissions, and enhance role assignment in UserRepository

--- app/Http/Resources/UserResource.php
+++ app/Http/Resources/UserResource.php
@@ -26,10 +26,18 @@ class UserResource extends JsonResource
             'organization_id' => $this->organization_id,
             'is_organization_active' => $this->organization?->is_active,
             'is_super_admin' => $this->role === UserRole::SUPER_ADMIN,
-            'roles' => $this->roles,
-            'permissions' => $this->getAllPermissions(),
+            'roles' => $this->roles->pluck('name')->all(),
+            'permissions' => $this->formattedPermissions(),
             'created_at' => $this->created_at->toIso8601String(),
             'updated_at' => $this->updated_at->toIso8601String(),
         ];
+    }
+
+    private function formattedPermissions(): array
+    {
+        return $this->getAllPermissions()->groupBy([
+            fn ($p) => explode('.', $p->name)[0], // Module
+            fn ($p) => explode('.', $p->name)[1], // Screen
+        ])->all();
     }
 }
